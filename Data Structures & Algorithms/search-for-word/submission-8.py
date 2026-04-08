a0f00b4dc1005ac1a74d27d5f579b0a207d8e3a3
yashverma9 <53463512+yashverma9@users.noreply.github.com>
2026-04-08 13:09:02 +0530

    Add: search-for-word - submission-8

---
+++ Data Structures & Algorithms/search-for-word/submission-8.py
@@ -0,0 +1,40 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+       # Optimal
+       # Time - O(m*n*4^L)
+       # Space - O(L)
+        '''
+        Instead of maintaining a visited store, we just replace the matrix inplace with a
+        random char to check if its visited or not. Instead of storing entire string, we
+        keep matching char by char and return True once we reach entire length of the word
+        '''
+        row = len(board)
+        col = len(board[0])
+
+        def backtrack(i, j, curInd):
+            if curInd == len(word):
+                return True
+
+            if i == row or i < 0 or j == col or j < 0 or board[i][j] != word[curInd]:
+                return False
+            
+            temp = board[i][j]
+            board[i][j] = "-"
+
+            found = (
+                backtrack(i+1, j, curInd + 1) or
+                backtrack(i-1, j, curInd + 1) or 
+                backtrack(i, j+1, curInd + 1) or
+                backtrack(i, j-1, curInd + 1)
+            )
+            board[i][j] = temp
+            return found
+            
+            
+        for i in range(row):
+            for j in range(col):
+                if board[i][j] == word[0]:
+                    if backtrack(i, j, 0):
+                        return True
+
+        return False
