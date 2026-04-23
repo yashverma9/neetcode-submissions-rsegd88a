2dfa956626a72a042facd905aa9576daf7ada2e9
yashverma9 <53463512+yashverma9@users.noreply.github.com>
2026-04-23 18:00:46 +0530

    Add: house-robber - submission-7

---
+++ Data Structures & Algorithms/house-robber/submission-7.py
@@ -0,0 +1,13 @@
+class Solution:
+    # Optimal - DP - Bottom-up
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        dp = [-1 for _ in range(n+2)]
+        dp[n] = 0 # End of house
+        dp[n+1] = 0 # End of house + 1 to manage i+2 jump
+        
+        for i in range(n-1, -1, -1):
+            dp[i] = max(nums[i] + dp[i+2], dp[i+1])
+        
+        return dp[0]
