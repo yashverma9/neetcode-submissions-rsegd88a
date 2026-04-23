b50032f180d312c619b12554760dc868b72f3edd
yashverma9 <53463512+yashverma9@users.noreply.github.com>
2026-04-24 00:51:54 +0530

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    # Optimal - Similar to longest palindrome substring
+    def countSubstrings(self, s: str) -> int:
+
+        res = 0
+        n = len(s)
+
+        def checkP(l, r):
+            nonlocal res
+            while l >= 0 and r < n and s[l] == s[r]:
+                res += 1
+            
+                l -= 1
+                r += 1
+    
+        for mid in range(n):
+            # odd
+            l, r = mid, mid
+            checkP(l,r)
+
+            # even
+            l, r = mid, mid+1
+            checkP(l,r)
+        
+        return res
