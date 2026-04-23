96e71c43dad4c818f1cac7950d1f4abc4c63d513
yashverma9 <53463512+yashverma9@users.noreply.github.com>
2026-04-23 17:46:44 +0530

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    # Optimal - DP - Top-down
+    def rob(self, nums: List[int]) -> int:
+        memo = {}
+
+        n = len(nums)
+
+        def jump(i):
+            if i >= n:
+                return 0
+            
+            if i in memo:
+                return memo[i]
+
+            rob = nums[i] + jump(i+2)
+
+            skip = jump(i+1)
+
+            memo[i] = max(rob, skip)
+            return memo[i]
+
+        return jump(0)
