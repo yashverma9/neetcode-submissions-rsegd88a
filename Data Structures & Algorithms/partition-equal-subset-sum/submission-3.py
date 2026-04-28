8ce47cd460b707cfaede18b540d2b13bcb555932
yashverma9 <53463512+yashverma9@users.noreply.github.com>
2026-04-28 22:35:27 +0530

    Add: partition-equal-subset-sum - submission-3

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    # Optimal - DP - Top-down
+    def canPartition(self, nums: List[int]) -> bool:
+        n = len(nums)
+        
+        total = sum(nums)
+        if total % 2 != 0:
+            return False
+        
+        target = total//2
+        
+        memo = {} # memo[(i, sum)]
+
+        def formSubset(i, curSum):
+            if curSum == target:
+                return True
+
+            if i >= n or curSum > target:
+                return False
+
+            if (i, curSum) in memo:
+                return memo[(i,curSum)]
+            
+            res = formSubset(i+1, curSum + nums[i]) or formSubset(i+1, curSum)
+            memo[(i, curSum)] = res
+            return res
+        
+        return formSubset(0,0)
+
+                   
