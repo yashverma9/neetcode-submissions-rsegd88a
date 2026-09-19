94d7cb65f7860b209acae17a71030321a827a711
yashverma9 <53463512+yashverma9@users.noreply.github.com>
2026-09-19 16:50:10 +0530

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Optimal
+
+        seen = {}
+
+        for i in range(len(nums)):
+            if target - nums[i] in seen:
+                return [seen[target-nums[i]], i]
+            
+            seen[nums[i]] = i
+        
+            
